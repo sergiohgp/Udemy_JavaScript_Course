245b0cc32be0efec05e6611570e4e06bfd5b7028
Sergio Pereira <sergiohgp@gmail.com>
2020-04-19 11:58:34 -0400

    Arrays with objects

--- arrays/notes-objects.js
+++ arrays/notes-objects.js
@@ -6,15 +6,22 @@ const notes = [{}, {
     body: 'I would like to review subjects'
 }, {
     title: 'My next lunch',
-    body: 'I would like eat pasta'
+    body: 'I would like to eat pasta'
 }]
 
-console.log(notes.length)
-console.log(notes)
 
-const index = notes.findIndex(function (note) {
-    console.log(note)
-    return note.title === 'My next trip'
-})
+const findNote = function (notes, noteTitle) {
+    return note = notes.find(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
 
-console.log(index)
+// const findNote = function (notes, noteTitle) {
+//     const index = notes.findIndex(function (note, index) {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[index]
+// }
+
+const note = findNote(notes, 'My next lunch')
+console.log(note)
--- arrays/toDo.js
+++ arrays/toDo.js
@@ -1,21 +1,60 @@
 // Create an array with five todos
-const todo = ['Shopping', 'walk', 'children pickup', 'study', 'meeting']
+const todo = [
+    {
+    text:'Shopping',
+    completed: true
+}, 
+{
+    text: 'walk',
+    completed: false
+}, 
+{
+    text: 'children pickup',
+    completed: true
+}, 
+{
+    text: 'study',
+    completed: true
+},
+{
+    text: 'meeting',
+    completed: false
+}]
 
-todo.splice(2, 1)
-todo[todo.length] = 'New todo'
-todo.shift()
+// todo.splice(2, 1)
+// todo[todo.length] = 'New todo'
+// todo.shift()
 
-console.log(`Yoy have ${todo.length} things to do in your list: `)
+// console.log(`Yoy have ${todo.length} things to do in your list: `)
 
 
-const todoList = function () {
-    for (let i = 0; i < todo.length; i++) {
-        console.log(`To do ${i + 1}: ${todo[i]}`)
+// const todoList = function () {
+//     for (let i = 0; i < todo.length; i++) {
+//         console.log(`To do ${i + 1}: ${todo[i]}`)
+//     }
+// }
+
+// todo.forEach(function (i) {
+//     console.log(i)
+// })
+
+// todoList()
+
+// Chalenge notes-object
+// 1. Covert todo array in an array of objects -> text, completed (true or false)
+// 2. create function to remove the todo by text value
+
+const deleteTodo = function(todos, text) {
+    const index = todos.findIndex(function(todo, index) {
+        return todo.text.toLowerCase() === text.toLowerCase()
+    })
+    if (index > -1){
+        todos.splice(index, 1, `item removed`)
     }
+
+
 }
 
-todo.forEach(function (i) {
-    console.log(i)
-})
-
-todoList()
+console.log(todo)
+deleteTodo(todo, 'study')
+console.log(todo)
